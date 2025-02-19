1d20cfaf32637c06dcc2f7b198c3d5e6c5ff3416
Yassine EL OUAFI <yelouafi@gmail.com>
2025-02-19 13:12:42 +0100

    remove unused

--- src/index.js
+++ src/index.js
@@ -9,53 +9,6 @@ import { syncUp } from "./sync.js";
 import { checkout } from "./checkout.js";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const apiClient = new ApiClient();
-
-function isLocalScript(data) {
-  return data.kit === "local" && data.uri.startsWith("./");
-}
-
-async function createProject(data, targetDir) {
-  // Copy template files
-  const templateDir = path.join(__dirname, "..", "template");
-  await fs.copy(templateDir, targetDir);
-
-  // Update package.json with game data
-  const packageJsonPath = path.join(targetDir, "package.json");
-  const packageJson = await fs.readJson(packageJsonPath);
-  packageJson.metadata = {
-    awe: { gameId: data.gameId },
-  };
-  await fs.writeJson(packageJsonPath, packageJson, { spaces: 2 });
-
-  // Create src directory if it doesn't exist
-  const srcDir = path.join(targetDir, "src");
-  await fs.ensureDir(srcDir);
-
-  // Write game files
-  for (const file of data.scripts) {
-    let uri = file.uri;
-    // for now only local scripts are supported
-    if (!isLocalScript(file)) {
-      continue;
-    }
-    // complete with default extension tsx if missing (js, ts, jsx, tsx)
-    const extensions = [".js", ".ts", ".jsx", ".tsx"];
-    const extension = extensions.find((ext) => uri.endsWith(ext));
-    if (!extension) {
-      uri += ".tsx";
-    }
-    const filePath = path.join(srcDir, uri);
-    await fs.writeFile(filePath, file.code);
-  }
-
-  // Run npm install in the target directory
-  console.log("Installing dependencies...");
-  const { execSync } = await import("child_process");
-  execSync("npm install", { stdio: "inherit", cwd: targetDir });
-}
 
 const program = new Command();
 
@@ -64,7 +17,7 @@ program.name("awe").description("CLI tool to fetch and create game projects");
 program
   .command("checkout")
   .argument("<gameId>", "ID of the game to checkout")
-  .action(async (gameId, options) => {
+  .action(async (gameId) => {
     try {
       console.log(`Checking out game ${gameId}...`);
       await checkout({ gameId });
@@ -77,7 +30,7 @@ program
 program
   .command("push")
   .description("Push local changes to remote game")
-  .action(async (options) => {
+  .action(async () => {
     try {
       await syncUp();
     } catch (error) {
